b444e952fa7d3a78d060d7d0c645ade2162db983
Beatriz García <30807322+BeatrizGarciaR@users.noreply.github.com>
2023-11-28 19:12:15 -0600

    New model Scenario Experiments
    Same parameters from previous model

--- Codes/Tesis/ModeloBase_PartialCoverage/Obj_Zs/Obj_NuevoModelo.py
+++ Codes/Tesis/ModeloBase_PartialCoverage/Obj_Zs/Obj_NuevoModelo.py
@@ -26,24 +26,25 @@ import xlwt
 # tamaños_L = [16, 30, 50, 70, 100]
 # tamaños_S = [150, 200]
 
-tamaños_I = [168] 
+tamaños_I = [168, 270, 500, 900, 1500] 
 tamaños_L = [16]
-tamaños_S = [10]
+tamaños_S = [10, 50, 100, 150, 200]
 
 K = [1,2]
 
-timelim = 1800 #5 horas 
+timelim = 10800 #3 horas 
 rates = [0.4]
+verif = 0.4
 
-eta = [10, 6]
+ambulance = [[10, 6], [20,11], [35,20]]
 t = 10
-tmax = 25
-w_vars = [1, 0.9]
+tmax = 30
+w_vars = [0.7, 0.3]
 
 countcsv = 1
        
 book=xlwt.Workbook(encoding="utf-8",style_compression=0)
-sheet = book.add_sheet('Tesis_NewModel_NewModel_111123', cell_overwrite_ok=True)
+sheet = book.add_sheet('Tesis_NewModel_NewModel_161123', cell_overwrite_ok=True)
 
 def data_cb(m, where):
     if where == gp.GRB.Callback.MIPSOL:
@@ -67,12 +68,15 @@ def data_cb(m, where):
 for iconj in range(len(tamaños_I)):
     for jconj in range(len(tamaños_L)):
         for sconj in range(len(tamaños_S)):
-            for verif in rates:
+            for k in range(len(ambulance)):
             #for rep in range(repeticiones):
                 
                 initial_time = time.time()
+                
+                eta = ambulance[k]
     
                 #Nombre: Instancias_Prueba_I_L_M_N_S_Rep
+                
                 
                 
                 archivo = open('Instances_DemandFixed_'
@@ -417,7 +421,7 @@ for iconj in range(len(tamaños_I)):
                 
                 #imprimir variables 
                 
-                with open('data_NewModel_NewModel_111123_'+str(len(I))+str('_')
+                with open('data_NewModel_NewModel_161123_'+str(len(I))+str('_')
                               +str(len(L))+str('_')
                               #+str(len(K))+str('_')
                               #+str(len(N))+str('_')
@@ -454,16 +458,16 @@ for iconj in range(len(tamaños_I)):
                 
                 #Nombre: Resultados_I_L_M_N_S
                 
-                model.write('model_NewModel_NewModel_111123_'+str(len(I))+str('_')
+                model.write('model_NewModel_NewModel_161123_'+str(len(I))+str('_')
                               +str(len(L))+str('_')
                               #+str(len(K))+str('_')
                               #+str(len(N))+str('_')
                               +str(len(S))+'_'+str(eta[0])+'_'+str(eta[1])+'.lp')
-                model.write('model_NewModel_NewModel_111123_'+str(len(I))+str('_')
+                model.write('model_NewModel_NewModel_161123_'+str(len(I))+str('_')
                               +str(len(L))+str('_')
                               +str(len(S))+'_'+str(eta[0])+'_'+str(eta[1])+'.mps')
                 
-                f = open ('Resultados_Prueba_NewModel_NewModel_111123_'
+                f = open ('Resultados_Prueba_NewModel_NewModel_161123_'
                               +str(len(I))+str('_')
                               +str(len(L))+str('_')
                               #+str(len(K))+str('_')
@@ -527,4 +531,4 @@ for iconj in range(len(tamaños_I)):
                 countcsv = countcsv + 1
                 
                 
-book.save('Tesis_NewModel_NewModel_111123_'+str(eta[0])+'_'+str(eta[1])+'.xls') 
+                book.save('Tesis_NewModel_NewModel_161123_'+str(eta[0])+'_'+str(eta[1])+'.xls') 
