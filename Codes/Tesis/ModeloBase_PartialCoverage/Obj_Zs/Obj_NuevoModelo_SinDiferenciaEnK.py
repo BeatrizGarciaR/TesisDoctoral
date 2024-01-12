80c02d61431f356e9469cf40011e222f9147efe5
BeatrizGarciaR <30807322+BeatrizGarciaR@users.noreply.github.com>
2024-01-12 02:12:37 -0600

    new model
    tal cual está en el overleaf

--- Codes/Tesis/ModeloBase_PartialCoverage/Obj_Zs/Obj_NuevoModelo_SinDiferenciaEnK.py
+++ Codes/Tesis/ModeloBase_PartialCoverage/Obj_Zs/Obj_NuevoModelo_SinDiferenciaEnK.py
@@ -27,13 +27,13 @@ import xlwt
 # tamaños_L = [16, 30, 50, 70, 100]
 # tamaños_S = [10, 50, 100, 150, 200]
 
-tamaños_I = [168] #Aquí batalla pero sí lo hace aún
-tamaños_L = [16]
-tamaños_S = [10]
+# tamaños_I = [168] #Aquí batalla pero sí lo hace aún
+# tamaños_L = [16]
+# tamaños_S = [10]
 
-# tamaños_I = [270, 500, 900, 1500] 
-# tamaños_L = [30, 50, 70, 100, 250]
-# tamaños_S = [10, 50, 100, 150, 200]
+tamaños_I = [168, 270, 500, 900, 1500] 
+tamaños_L = [16, 30, 50, 70, 100, 250]
+tamaños_S = [10, 50, 100, 150, 200]
 
 K = [1,2]
 
