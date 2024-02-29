d9d92a2c4c3117e73a9e4f4abe9effa41048308f
Beatriz García <30807322+BeatrizGarciaR@users.noreply.github.com>
2024-02-28 20:21:47 -0600

    Experiments 280224

--- Codes/Tesis/ModeloBase_PartialCoverage/Obj_Zs/InstanciasTS.R
+++ Codes/Tesis/ModeloBase_PartialCoverage/Obj_Zs/InstanciasTS.R
@@ -5,7 +5,7 @@
 
 tamanos_I <- c(168, 270, 500, 900, 1500)
 tamanos_L <- c(16, 30, 50, 70, 100)
-tamanos_S <- c(10, 50, 100, 150, 200)
+tamanos_S <- c(60, 70, 80, 90)
 # tamanos_I <- c(168)
 # tamanos_L <- c(16)
 # tamanos_S <- c(10)
