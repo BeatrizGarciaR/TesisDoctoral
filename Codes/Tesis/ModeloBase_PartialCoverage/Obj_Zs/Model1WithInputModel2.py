449ab19c4678ded7c0398d9bd9e721cf484c011c
Beatriz García <30807322+BeatrizGarciaR@users.noreply.github.com>
2024-03-05 23:51:40 -0600

    Experiments 280224

--- Codes/Tesis/ModeloBase_PartialCoverage/Obj_Zs/Model1WithInputModel2.py
+++ Codes/Tesis/ModeloBase_PartialCoverage/Obj_Zs/Model1WithInputModel2.py
@@ -28,9 +28,9 @@ import xlwt
 # tamaños_L = [16, 30, 50, 70, 100]
 # tamaños_S = [150, 200]
 
-tamaños_I = [168] 
-tamaños_L = [16]
-tamaños_S = [10]
+tamaños_I = [168, 270, 500, 900, 1500] 
+tamaños_L = [16, 30, 50, 70, 100]
+tamaños_S = [10, 50, 100, 150, 200]
 
 # tamaños_I = [168] 
 # tamaños_L = [16]
